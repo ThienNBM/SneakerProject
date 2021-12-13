88f577b1901344decce4a1dbe5460f9938d110be
ThienNBM <minhthien121198@gmail.com>
2021-12-13 21:48:18 +0700

    Inside: thêm phần xem danh sách đơn hàng theo người dùng

--- SneakerAPI/Controllers/OrderController.cs
+++ SneakerAPI/Controllers/OrderController.cs
@@ -38,5 +38,25 @@ namespace SneakerAPI.Controllers
             }
             return orders;
         }
+        
+        [HttpGet]
+        [Route("GetByUserId/{id}")]
+        public async Task<ActionResult<IEnumerable<OrderGetByUserId>>> GetByUserId(int id)
+        {
+            string StoredProc = "exec Order_GetByUserId @UserID, @ErrorCode OUTPUT, @ErrorMessage OUTPUT";
+            var UserID = new SqlParameter("@UserID", id);
+            var ErrorCode = new SqlParameter("@ErrorCode", System.Data.SqlDbType.NVarChar, 100) { Direction = System.Data.ParameterDirection.Output };
+            var ErrorMessage = new SqlParameter("@ErrorMessage", System.Data.SqlDbType.NVarChar, 4000) { Direction = System.Data.ParameterDirection.Output };
+            List<OrderGetByUserId> orders;
+            try
+            {
+                orders = await _context.OrderGetByUserId.FromSqlRaw(StoredProc, UserID, ErrorCode, ErrorMessage).ToListAsync();
+            }
+            catch
+            {
+                orders = null;
+            }
+            return orders;
+        }
     }
 }
