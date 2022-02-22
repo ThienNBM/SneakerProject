81c17175536e5f295d0d9fec20c9b17dd0af9b23
ThienNBM <minhthien121198@gmail.com>
2022-02-22 23:55:16 +0700

    commit

--- SneakerInside/wwwroot/js/site.js
+++ SneakerInside/wwwroot/js/site.js
@@ -2,3 +2,56 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//Hàm hiện modal thêm, sửa
+function showModal(url, name) {
+    $.ajax({
+        type: "GET",
+        url: url,
+        success: function (res) {
+            $("#commonModal .modal-title").html(name);
+            $("#commonModal .modal-body").html(res);
+            $("#commonModal").modal('show');
+        }
+    })
+}
+
+//Hàm thực hiện thêm, sửa
+function submitForm(form) {
+    $.validator.unobtrusive.parse(form);
+    if ($(form).valid()) {
+        $.ajax({
+            type: "POST",
+            url: form.action,
+            data: new FormData(form),
+            contentType: false,
+            processData: false,
+            success: function (res) {
+                if (res.isValid) {
+                    if (res.error.errorMessage == "") {
+                        $.bootstrapGrowl("Thực hiện thành công", {
+                            type: 'success'
+                        });
+                        $("#commonModal .modal-body").html('');
+                        $("#commonModal .modal-title").html('');
+                        $("#commonModal").modal('hide');
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        $.bootstrapGrowl(res.error.errorMessage, {
+                            type: 'danger'
+                        });
+                    }
+                }
+                else {
+                    setTimeout(function () {
+                        $.bootstrapGrowl("Dữ liệu không hợp lệ", {
+                            type: 'danger'
+                        });
+                    }, 3000);
+                }
+            }
+        });
+    }
+    return false;
+}
